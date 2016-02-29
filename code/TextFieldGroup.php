3b242d100752e1ee3691092639f9e01e1c25df2b
spekulatius <petert@catalyst.net.nz>
2016-02-29 14:27:53 +1300

    Remove trailing spaces in the source code

--- code/TextFieldGroup.php
+++ code/TextFieldGroup.php
@@ -2,14 +2,14 @@
 
 /**
  * This is intended to make any combination of text fields and labels on a single line.  Eg for URLSegments prepended by the domain as a label, or for X, Y and Z coordinates for a single location.
- * 
+ *
  * The differences from FieldGroup are
  * a) it takes away the minimum width of fields so things are spaced appropriately; and
  * b) if you pass in a string instead of a field it's automatically turned in to a LiteralField
- * 
+ *
  */
 class TextFieldGroup extends FieldGroup {
-	
+
 	function __construct($arg1 = null, $arg2 = null) {
 		if (is_array($arg1) || is_a($arg1, 'FieldSet')) {
 			$fields = $arg1;
@@ -20,13 +20,13 @@ class TextFieldGroup extends FieldGroup {
 			$fields = func_get_args();
 			if(!is_object(reset($fields))) $this->title = array_shift($fields);
 		}
-		
+
 		foreach($fields as $key => $field) {
 			if (is_string($field)) {
 				$fields[$key] = new LiteralField($this->Name().'Literal'.$key, $field);
 			}
 		}
-					
+
 		parent::__construct($fields);
 	}
 
@@ -36,8 +36,8 @@ class TextFieldGroup extends FieldGroup {
 	 */
 	function FieldHolder($properties = array()) {
 		Requirements::css(basename(dirname(__DIR__)) . '/css/TextFieldGroup.css');
-		
+
 		return parent::FieldHolder($properties);
 	}
-	
+
 }
